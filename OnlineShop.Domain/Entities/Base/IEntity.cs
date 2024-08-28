c9e1cbe17a494a591979d1ffb006c54369cb7739
Mehedi Hasan Kajol <hmehedi@julyservices.com>
2024-08-28 16:13:53 +0600

    project clean up

---
+++ OnlineShop.Domain/Entities/Base/IEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineShop.Domain.Entities.Base;
+
+public interface IEntity<TId>
+{
+    TId Id { get; }
+}
+
+public abstract class Entity<TId> : IEntity<TId>
+{
+    public required TId Id { get; set; }
+}
